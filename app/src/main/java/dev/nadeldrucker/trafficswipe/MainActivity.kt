787ebca64430a87f61ee936a118432689e505ed9
Philipp97714 <Philipp@Nadeldrucker.dev>
2019-10-26 00:38:53 +0200

    init

---
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.nadeldrucker.trafficswipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
