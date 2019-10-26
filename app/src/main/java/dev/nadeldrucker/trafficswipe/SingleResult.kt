57c756a0c4d36ccf45a9e0e39729d0bed3104b5c
Philipp97714 <Philipp@Nadeldrucker.dev>
2019-10-26 13:27:42 +0200

    added layout for result activity

---
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/SingleResult.kt
@@ -0,0 +1,12 @@
+package dev.nadeldrucker.trafficswipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_result)
+    }
+}
