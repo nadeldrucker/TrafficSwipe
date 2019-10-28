672671b2853911bab20530201fcf4db83eb59999
Paul Orlob <orlobpaul@gmail.com>
2019-10-28 19:08:43 +0100

    Transformed project to use java instead of kotlin

---
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.nadeldrucker.trafficswipe;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+}
