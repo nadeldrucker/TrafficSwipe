e3c4e3215f41feff0031b723a3ffd4a773068085
Paul Orlob <orlobpaul@gmail.com>
2019-10-28 21:11:15 +0100

    Initial navigation

---
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/HostNavActivity.java
@@ -0,0 +1,13 @@
+package dev.nadeldrucker.trafficswipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HostNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_nav);
+    }
+}
---
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/StartFragment.java
@@ -0,0 +1,19 @@
+package dev.nadeldrucker.trafficswipe;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StartFragment extends Fragment {
+
+    public StartFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+}
