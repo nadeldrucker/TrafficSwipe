2abfb5cddca05e27864a3dc9bf09fd73e841ee75
Paul Orlob <orlobpaul@gmail.com>
2019-10-29 02:49:15 +0100

    Added help fragment

--- app/src/main/java/dev/nadeldrucker/trafficswipe/StartFragment.java
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/StartFragment.java
@@ -1,10 +1,18 @@
 package dev.nadeldrucker.trafficswipe;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.navigation.Navigation;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class StartFragment extends Fragment {
 
@@ -16,4 +24,14 @@ public class StartFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_start, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        FloatingActionButton fabHelp = view.findViewById(R.id.startFragment_helpFab);
+        fabHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_startFragment_to_helpSheet);
+            }
+        });
+    }
 }
