ef661ed9dae53fc025983e0ac66eea2055b33c50
Philipp97714 <Philipp@Nadeldrucker.dev>
2019-10-29 18:14:18 +0100

    implemented broken animation button

--- app/src/main/java/dev/nadeldrucker/trafficswipe/StartFragment.java
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/StartFragment.java
@@ -1,15 +1,23 @@
 package dev.nadeldrucker.trafficswipe;
 
 import android.app.Dialog;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.navigation.Navigation;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
@@ -28,5 +36,14 @@ public class StartFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         FloatingActionButton fabHelp = view.findViewById(R.id.startFragment_helpFab);
         fabHelp.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_startFragment_to_helpSheet));
+        ((Button) view.findViewById(R.id.button)).setOnClickListener(v -> {
+            ImageView imageView = v.findViewById(R.id.aivBrain);
+            //imageView.setBackgroundResource(R.drawable.ic_brain);
+            Drawable drawable = imageView.getDrawable();
+            if (drawable instanceof Animatable) {
+                ((Animatable) drawable).start();
+            }
+        });
     }
+
 }
