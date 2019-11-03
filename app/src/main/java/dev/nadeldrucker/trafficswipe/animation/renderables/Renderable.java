5d4610771eeacf57568a1fc0eb21296851096e5c
Paul Orlob <orlobpaul@gmail.com>
2019-11-04 00:15:29 +0100

    Added documentation

--- app/src/main/java/dev/nadeldrucker/trafficswipe/animation/renderables/Renderable.java
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/animation/renderables/Renderable.java
@@ -6,6 +6,14 @@ import android.graphics.Canvas;
  * Class representing a renderable object
  */
 public abstract class Renderable {
+    /**
+     * Renders element onto canvas.
+     * @param canvas canvas to draw element onto
+     */
     public abstract void render(Canvas canvas);
+
+    /**
+     * Updates element (eg. position)
+     */
     public abstract void update();
 }
