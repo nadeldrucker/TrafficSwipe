58186fff2ea30c8113d6228db5b6c72c3f1869ee
Paul Orlob <orlobpaul@gmail.com>
2019-11-20 22:01:47 +0100

    Refactored api interface into abstract entrypoint

--- app/src/main/java/dev/nadeldrucker/trafficswipe/dao/transport/model/data/Entrypoint.java
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/dao/transport/model/data/Entrypoint.java
@@ -2,7 +2,9 @@ package dev.nadeldrucker.trafficswipe.dao.transport.model.data;
 
 import com.android.volley.RequestQueue;
 import dev.nadeldrucker.jvvo.Models.Stop;
+import dev.nadeldrucker.trafficswipe.dao.transport.model.data.vehicle.AbstractVehicle;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -31,5 +33,32 @@ public abstract class Entrypoint {
      * @return future throwing an exception if an error occurs
      */
     public abstract CompletableFuture<Route> getRoute(Station start, Station destination);
+
+    /**
+     * Provide routes from online service
+     *
+     * @param from start point for route
+     * @param to   target for route
+     * @return future of a list of routes, null if no route is available
+     */
+    public abstract CompletableFuture<List<Route>> getRoutes(Station from, Station to);
+
+    /**
+     * Get all departures from a stop
+     *
+     * @param from stop
+     * @return future of departures from stop
+     */
+    public abstract CompletableFuture<List<AbstractVehicle>> getDepartures(Station from);
+
+    /**
+     * Get all departures from a stop that also stops at a specific point / has the direction to
+     *
+     * @param from start point
+     * @param over vehicle must stop at least here
+     * @return future of list of all vehicles that match the query
+     */
+    public abstract CompletableFuture<List<AbstractVehicle>> getDepartures(Station from, Station over);
+
 
 }
