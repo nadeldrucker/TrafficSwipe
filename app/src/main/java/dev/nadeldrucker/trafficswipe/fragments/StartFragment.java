479ac8f5b9e666b3a873ca7cd0e2de241c98c1dc
Philipp97714 <Philipp@Nadeldrucker.dev>
2019-11-24 19:28:30 +0100

    start search after 3 chars without pressing ok

--- app/src/main/java/dev/nadeldrucker/trafficswipe/fragments/StartFragment.java
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/fragments/StartFragment.java
@@ -2,27 +2,32 @@ package dev.nadeldrucker.trafficswipe.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import dev.nadeldrucker.trafficswipe.R;
 
 import java.util.Objects;
+
+import dev.nadeldrucker.trafficswipe.R;
 
 public class StartFragment extends Fragment {
 
     private static final String TAG = "StartFragment";
     public static final String BUNDLE_QUERY = "query";
+
 
     public StartFragment() {
     }
@@ -30,6 +35,7 @@ public class StartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_start, container, false);
+
     }
 
     @Override
@@ -50,6 +56,29 @@ public class StartFragment extends Fragment {
                 return true;
             }
             return false;
+        });
+        etSearch.addTextChangedListener(new TextWatcher() {
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (etSearch.getText().toString().length() >= 3) {
+                    Log.d(TAG, "edit text is done! " + etSearch.getText().toString());
+                    setSoftKeyboardState(false, etSearch);
+                    showDepartureTable(etSearch.getText().toString());
+                    etSearch.clearFocus();
+                    etSearch.getText().clear();
+                }
+            }
         });
 
         final FloatingActionButton fabSearch = view.findViewById(R.id.startFragment_searchFab);
