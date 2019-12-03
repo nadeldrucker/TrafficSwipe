5f736a513139d6cdd5cbe440f3901b0b1403ab6f
Paul Orlob <orlobpaul@gmail.com>
2019-12-03 01:21:28 +0100

    Fixed #34 - added character gesture recognition

---
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/inference/CharacterRecognizer.java
@@ -0,0 +1,210 @@
+package dev.nadeldrucker.trafficswipe.inference;
+
+import android.content.res.AssetFileDescriptor;
+import android.graphics.*;
+import android.util.Log;
+import dev.nadeldrucker.trafficswipe.App;
+import dev.nadeldrucker.trafficswipe.animation.renderables.TouchPath;
+import dev.nadeldrucker.trafficswipe.dao.gestures.TouchCoordinate;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+/**
+ * Class for inferring handwritten characters
+ */
+public class CharacterRecognizer {
+
+    private static CharacterRecognizer instance;
+    private static final String MODEL_PATH = "model.tflite";
+    private static String TAG = "CharacterRecognizer";
+
+    private final ByteBuffer buffer;
+    private final Interpreter tflite;
+
+    private static final int PRESCALE_WIDTH = 128, PRESCALE_HEIGHT = 128;
+    private static final int WIDTH = 28, HEIGHT = 28;
+    private static final int STROKE_WIDTH = 6;
+    private static final float PADDING = 0.15f;
+    private static final int LABEL_SIZE = 26;
+
+    private CharacterRecognizer() throws IOException {
+        tflite = new Interpreter(loadModelFile(), new Interpreter.Options().setUseNNAPI(true));
+        tflite.getInputTensor(0);
+        buffer = ByteBuffer.allocateDirect(4 * WIDTH * HEIGHT);
+        buffer.order(ByteOrder.nativeOrder());
+    }
+
+    /**
+     * Memory-map the model file in Assets.
+     */
+    private static ByteBuffer loadModelFile() throws IOException {
+        AssetFileDescriptor fileDescriptor = App.getContext().getAssets().openFd(MODEL_PATH);
+
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    /**
+     * retrieves an instance of the character recognizer
+     *
+     * @return instance
+     */
+    public static CharacterRecognizer getInstance() {
+        if (instance == null) {
+            try {
+                instance = new CharacterRecognizer();
+            } catch (IOException e) {
+                Log.e(TAG, "Error while creating instance!", e);
+            }
+        }
+
+        return instance;
+    }
+
+    private void putBitmapInBuffer(Bitmap bitmap) {
+        int[] imagePixels = new int[WIDTH * HEIGHT];
+
+        bitmap.getPixels(imagePixels, 0, bitmap.getWidth(), 0, 0,
+                bitmap.getWidth(), bitmap.getHeight());
+
+        buffer.rewind();
+
+        int pixel = 0;
+        for (int i = 0; i < WIDTH; ++i) {
+            for (int j = 0; j < HEIGHT; ++j) {
+                final int val = imagePixels[pixel++];
+                buffer.putFloat(convertToGreyScale(val));
+            }
+        }
+    }
+
+    public char infer(List<List<TouchCoordinate>> path) {
+        Bitmap bitmapFromPaths = createBitmapFromPaths(path);
+        putBitmapInBuffer(bitmapFromPaths);
+
+        float[][] outputArray = new float[1][LABEL_SIZE];
+        tflite.run(buffer, outputArray);
+
+        float maxIndex = 0;
+        float max = Float.MIN_VALUE;
+        for (int i = 0; i < outputArray[0].length; i++) {
+            if (max < outputArray[0][i]) {
+                max = outputArray[0][i];
+                maxIndex = i;
+            }
+        }
+
+        return indexInAlphabetToChar((int) maxIndex);
+    }
+
+    private Bitmap createBitmapFromPaths(List<List<TouchCoordinate>> rawPaths) {
+        Bitmap bitmap = Bitmap.createBitmap(PRESCALE_WIDTH, PRESCALE_HEIGHT, Bitmap.Config.ARGB_8888);
+
+        Canvas c = new Canvas(bitmap);
+        c.drawColor(Color.BLACK);
+
+        List<List<TouchCoordinate>> paths = preprocessPaths(rawPaths, PRESCALE_HEIGHT);
+
+        paths.forEach(path -> {
+            Path p = new Path();
+
+            for (int i = 0; i < path.size(); i++) {
+                TouchCoordinate coord = path.get(i);
+
+                if (i == 0) {
+                    p.moveTo(coord.getX(), coord.getY());
+                } else {
+                    p.lineTo(coord.getX(), coord.getY());
+                }
+            }
+
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(STROKE_WIDTH);
+            paint.setAntiAlias(true);
+            paint.setColor(Color.WHITE);
+
+            c.drawPath(p, paint);
+        });
+
+        return Bitmap.createScaledBitmap(bitmap, WIDTH, HEIGHT, true);
+    }
+
+    private List<List<TouchCoordinate>> preprocessPaths(List<List<TouchCoordinate>> paths, float scale) {
+        float maxX = paths.stream().flatMap(List::stream).max(Comparator.comparing(TouchCoordinate::getX)).map(TouchCoordinate::getX).orElse(Float.MAX_VALUE);
+        float maxY = paths.stream().flatMap(List::stream).max(Comparator.comparing(TouchCoordinate::getY)).map(TouchCoordinate::getY).orElse(Float.MAX_VALUE);
+
+        float minX = paths.stream().flatMap(List::stream).min(Comparator.comparing(TouchCoordinate::getX)).map(TouchCoordinate::getX).orElse(Float.MIN_VALUE);
+        float minY = paths.stream().flatMap(List::stream).min(Comparator.comparing(TouchCoordinate::getY)).map(TouchCoordinate::getY).orElse(Float.MIN_VALUE);
+
+        float width = maxX - minX;
+        float height = maxY - minY;
+
+        float padW = width * PADDING;
+        float padH = height * PADDING;
+
+        maxX += padW;
+        minX -= padW;
+
+        maxY += padH;
+        minY -= padH;
+
+        float xIncrease = 0;
+        float yIncrease = 0;
+
+        if (height > width) {
+            xIncrease += height - width;
+        } else {
+            yIncrease += width - height;
+        }
+
+        float finalXIncrease = xIncrease;
+        float finalMinX = minX;
+        float finalMaxX = maxX;
+        float finalYIncrease = yIncrease;
+        float finalMinY = minY;
+        float finalMaxY = maxY;
+
+        return paths.stream()
+                .map(touchCoordinates -> touchCoordinates
+                        .stream()
+                        .map(touchCoordinate -> {
+                            float normalizedX = normalize(touchCoordinate.getX() + finalXIncrease / 2, finalMinX, finalMaxX + finalXIncrease) * scale;
+                            float normalizedY = normalize(touchCoordinate.getY() + finalYIncrease / 2, finalMinY, finalMaxY + finalYIncrease) * scale;
+                            return new TouchCoordinate(normalizedX, normalizedY);
+                        })
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+    }
+
+    private static float normalize(float val, float min, float max) {
+        return (val - min) / (max - min);
+    }
+
+    private static float convertToGreyScale(int color) {
+        float r = ((color >> 16) & 0xFF);
+        float g = ((color >> 8) & 0xFF);
+        float b = ((color) & 0xFF);
+
+        int grayscaleValue = (int) (0.299f * r + 0.587f * g + 0.114f * b);
+        return grayscaleValue / 255.0f;
+    }
+
+    private static char indexInAlphabetToChar(int index) {
+        final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        return alphabet.charAt(index);
+    }
+}
---
+++ app/src/main/java/dev/nadeldrucker/trafficswipe/ui/CharacterDrawView.java
@@ -0,0 +1,56 @@
+package dev.nadeldrucker.trafficswipe.ui;
+
+import android.content.Context;
+import android.os.health.TimerStat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import dev.nadeldrucker.trafficswipe.animation.TouchPathView;
+import dev.nadeldrucker.trafficswipe.dao.gestures.TouchCoordinate;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Consumer;
+
+public class CharacterDrawView extends TouchPathView {
+
+    public static final int DELAY = 500;
+    private boolean hasDrawnSinceLastTimer = true;
+    private Consumer<List<List<TouchCoordinate>>> listener;
+    private TimerTask lastTask;
+
+
+    public CharacterDrawView(Context context) {
+        super(context);
+    }
+
+    public CharacterDrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            hasDrawnSinceLastTimer = false;
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    if (listener != null && !hasDrawnSinceLastTimer) listener.accept(getTouchPaths());
+                }
+            };
+            lastTask = task;
+            new Timer().schedule(task, DELAY);
+        } else {
+            if (lastTask != null) lastTask.cancel();
+            hasDrawnSinceLastTimer = true;
+        }
+        return true;
+
+    }
+
+    public void setListener(Consumer<List<List<TouchCoordinate>>> listener) {
+        this.listener = listener;
+    }
+}
