f3eaa0254c5eea465915b10cbfb21fcf90c35b43
Paul Orlob <orlobpaul@gmail.com>
2019-12-15 23:48:19 +0100

    Fixed tests

--- app/src/test/java/dev/nadeldrucker/trafficswipe/dao/transport/apis/vvo/VvoStationTest.java
+++ app/src/test/java/dev/nadeldrucker/trafficswipe/dao/transport/apis/vvo/VvoStationTest.java
@@ -34,32 +34,33 @@ public class VvoStationTest extends AbstractVolleyMockApiTest {
      */
     @Test
     public void getStops() throws ExecutionException, InterruptedException {
-        System.out.println("Current timezone: " + ZoneId.systemDefault().toString());
-
         mockHttpStack.queueNextResponse("mocks/vvo/GETDepartures.json");
-        List<Map.Entry<Vehicle, DepartureTime>> list = vvoStation.getDepartures().get().entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toList());
 
-        // check 8 direction gorbitz (first entry)
-        Map.Entry<Vehicle, DepartureTime> gorbitz8 = list.get(0);
-        assertEquals("voe:11008: :H:j19", gorbitz8.getKey().getEntityId());
-        assertEquals(LocalDateTime.of(2019, 12, 12, 21, 38, 0), gorbitz8.getValue().getDepartureTimeWithDelay().toLocalDateTime());
-        assertEquals(Duration.of(0, ChronoUnit.SECONDS), gorbitz8.getValue().getDelay());
-        assertTrue(gorbitz8.getKey() instanceof Tram);
-        assertEquals("8", gorbitz8.getKey().getLineId());
+        waitForWrappedLiveData(vvoStation.getDepartures(), vehicleDepartureTimeMap -> {
+            List<Map.Entry<Vehicle, DepartureTime>> list = vehicleDepartureTimeMap.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .collect(Collectors.toList());
 
-        // check a departure that is delayed
-        Map.Entry<Vehicle, DepartureTime> wilderMann3 = list.stream()
-                .filter(entry -> entry.getKey().getEntityId().equals("voe:11003: :H:j19"))
-                .findAny()
-                .orElseThrow(RuntimeException::new);
-        assertEquals("voe:11003: :H:j19", wilderMann3.getKey().getEntityId());
-        assertEquals("3", wilderMann3.getKey().getLineId());
-        assertTrue(wilderMann3.getKey() instanceof Tram);
-        assertEquals(LocalDateTime.of(2019, 12, 12, 21, 44, 0), wilderMann3.getValue().getDeparture().toLocalDateTime());
-        assertEquals(Duration.of(1, ChronoUnit.MINUTES), wilderMann3.getValue().getDelay());
-        assertEquals(LocalDateTime.of(2019, 12, 12, 21, 45, 0), wilderMann3.getValue().getDepartureTimeWithDelay().toLocalDateTime());
+            // check 8 direction gorbitz (first entry)
+            Map.Entry<Vehicle, DepartureTime> gorbitz8 = list.get(0);
+            assertEquals("voe:11008: :H:j19", gorbitz8.getKey().getEntityId());
+            assertEquals(LocalDateTime.of(2019, 12, 12, 21, 38, 0), gorbitz8.getValue().getDepartureTimeWithDelay().toLocalDateTime());
+            assertEquals(Duration.of(0, ChronoUnit.SECONDS), gorbitz8.getValue().getDelay());
+            assertTrue(gorbitz8.getKey() instanceof Tram);
+            assertEquals("8", gorbitz8.getKey().getLineId());
+
+            // check a departure that is delayed
+            Map.Entry<Vehicle, DepartureTime> wilderMann3 = list.stream()
+                    .filter(entry -> entry.getKey().getEntityId().equals("voe:11003: :H:j19"))
+                    .findAny()
+                    .orElseThrow(RuntimeException::new);
+            assertEquals("voe:11003: :H:j19", wilderMann3.getKey().getEntityId());
+            assertEquals("3", wilderMann3.getKey().getLineId());
+            assertTrue(wilderMann3.getKey() instanceof Tram);
+            assertEquals(LocalDateTime.of(2019, 12, 12, 21, 44, 0), wilderMann3.getValue().getDeparture().toLocalDateTime());
+            assertEquals(Duration.of(1, ChronoUnit.MINUTES), wilderMann3.getValue().getDelay());
+            assertEquals(LocalDateTime.of(2019, 12, 12, 21, 45, 0), wilderMann3.getValue().getDepartureTimeWithDelay().toLocalDateTime());
+        });
     }
 
 
